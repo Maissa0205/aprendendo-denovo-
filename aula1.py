48c5b8dd5dd594c04c730a0256a15b318f7f2be8
maissa0205 <maissaferreira378@gmail.com>
2025-01-22 14:59:09 -0400

    commit inicial

---
+++ aula1.py
@@ -0,0 +1,47 @@
+a=10
+b=2 
+c="wander Luiz"
+d=1.2
+#Comparando se as variaveis são iguais 
+print(a==b)
+
+#Comparando se as variaveis são diferentesrint(
+#print(a 1=b)
+#Testar se a variavel a é maior que b
+print(a>b)
+
+
+#Testar se a variavel é menor que b
+#Print(a<b)
+
+#Testar se a variavel a é maior ou igual que b
+#print(a>=b)
+
+#Testar se a variavel a é menor ou igual que b
+#print(a<=b)
+
+#Testar se o dobro da variavel a é maior ou igual a b 
+#print(2*>=b)
+
+#Testar a potenciação de um numero 
+print(2**3)
+#multiplicacão 
+#print(a/b)
+
+#Divisão Quociente
+#Print(a/b)
+ #Divisao Resto
+ #print(a%b)
+#Soma
+#print(a+b)
+
+#Subtracão 
+print(a-b)
+
+#Equação
+print(b**(2+1) + a %3)
+
+#Verificar tipo de variavel
+print(type(d))
+
+
